ac1e5ab826de9bec2e0b49504bdf60b6e5671c55
Claire Anita <anitachepkemoi1@gmail.com>
2026-02-24 09:10:09 +0300

    transcribe branch

---
+++ src/app/api/transcribe/route.ts
@@ -0,0 +1,10 @@
+// Use Server
+// import axios
+//Set up API key  from the .env
+// Define your headers - Set up authorization and content type
+// Set up Function  - dont forget to export  
+// Submit transcription request 
+// const transcript =  await  axios.post (url, )
+// do not console log  instead return {
+     // url: file.ufsUrl,
+    //};
